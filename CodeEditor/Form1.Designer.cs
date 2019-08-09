754ab0fa9a2132b13055c2ef06d5b0444f02a52d
FredTheNoob <43958385+FredTheNoob@users.noreply.github.com>
2019-08-09 18:01:54 +0200

    ADDED: functionality to the open file in the menu tab

--- CodeEditor/Form1.Designer.cs
+++ CodeEditor/Form1.Designer.cs
@@ -32,22 +32,34 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ccsprojToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textFiletxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pythonpyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveFileCTRLSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveFileAsCTRLSHIFTSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pythonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.javaScriptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.objectiveCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vBNetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.luaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pHPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.editToolStripMenuItem,
@@ -66,20 +78,8 @@
             this.saveFileCTRLSToolStripMenuItem,
             this.saveFileAsCTRLSHIFTSToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.fileToolStripMenuItem.Text = "File";
-            // 
-            // editToolStripMenuItem
-            // 
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
-            this.editToolStripMenuItem.Text = "Edit";
-            // 
-            // settingsToolStripMenuItem
-            // 
-            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.settingsToolStripMenuItem.Text = "Settings";
             // 
             // newFileToolStripMenuItem
             // 
@@ -88,44 +88,59 @@
             this.textFiletxtToolStripMenuItem,
             this.pythonpyToolStripMenuItem});
             this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
-            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(232, 22);
             this.newFileToolStripMenuItem.Text = "New File";
             // 
             // ccsprojToolStripMenuItem
             // 
             this.ccsprojToolStripMenuItem.Name = "ccsprojToolStripMenuItem";
-            this.ccsprojToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ccsprojToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.ccsprojToolStripMenuItem.Text = "C# (.csproj)";
             // 
             // textFiletxtToolStripMenuItem
             // 
             this.textFiletxtToolStripMenuItem.Name = "textFiletxtToolStripMenuItem";
-            this.textFiletxtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.textFiletxtToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.textFiletxtToolStripMenuItem.Text = "Text File (.txt)";
             // 
             // pythonpyToolStripMenuItem
             // 
             this.pythonpyToolStripMenuItem.Name = "pythonpyToolStripMenuItem";
-            this.pythonpyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pythonpyToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.pythonpyToolStripMenuItem.Text = "Python (.py)";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(232, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
             // 
             // saveFileCTRLSToolStripMenuItem
             // 
             this.saveFileCTRLSToolStripMenuItem.Name = "saveFileCTRLSToolStripMenuItem";
-            this.saveFileCTRLSToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.saveFileCTRLSToolStripMenuItem.Size = new System.Drawing.Size(232, 22);
             this.saveFileCTRLSToolStripMenuItem.Text = "Save File (CTRL+S)";
             // 
             // saveFileAsCTRLSHIFTSToolStripMenuItem
             // 
             this.saveFileAsCTRLSHIFTSToolStripMenuItem.Name = "saveFileAsCTRLSHIFTSToolStripMenuItem";
-            this.saveFileAsCTRLSHIFTSToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.saveFileAsCTRLSHIFTSToolStripMenuItem.Size = new System.Drawing.Size(232, 22);
             this.saveFileAsCTRLSHIFTSToolStripMenuItem.Text = "Save File As (CTRL+SHIFT+S)";
             // 
-            // openFileToolStripMenuItem
+            // editToolStripMenuItem
             // 
-            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
-            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
-            this.openFileToolStripMenuItem.Text = "Open File";
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
             // 
             // fastColoredTextBox1
             // 
@@ -143,24 +158,112 @@
         '\"',
         '\'',
         '\''};
-            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(179, 14);
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;]+);\r\n^\\s*(case|default)\\s*[^:]" +
+    "*(?<range>:)\\s*(?<range>[^;]+);\r\n";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(27, 14);
             this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BackColor = System.Drawing.Color.DimGray;
+            this.fastColoredTextBox1.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.fastColoredTextBox1.CaretColor = System.Drawing.Color.White;
             this.fastColoredTextBox1.CharHeight = 14;
             this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.CurrentLineColor = System.Drawing.Color.Silver;
             this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
-            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.fastColoredTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(180)))), ((int)(((byte)(92)))));
+            this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.DimGray;
             this.fastColoredTextBox1.IsReplaceMode = false;
             this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.CSharp;
+            this.fastColoredTextBox1.LeftBracket = '(';
+            this.fastColoredTextBox1.LeftBracket2 = '{';
+            this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(180)))), ((int)(((byte)(92)))));
             this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 27);
             this.fastColoredTextBox1.Name = "fastColoredTextBox1";
             this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
-            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.RightBracket = ')';
+            this.fastColoredTextBox1.RightBracket2 = '}';
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
             this.fastColoredTextBox1.Size = new System.Drawing.Size(800, 423);
             this.fastColoredTextBox1.TabIndex = 1;
-            this.fastColoredTextBox1.Text = "fastColoredTextBox1";
             this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cToolStripMenuItem1,
+            this.cToolStripMenuItem2,
+            this.cToolStripMenuItem,
+            this.javaScriptToolStripMenuItem,
+            this.luaToolStripMenuItem,
+            this.objectiveCToolStripMenuItem,
+            this.pythonToolStripMenuItem,
+            this.perlToolStripMenuItem,
+            this.pHPToolStripMenuItem,
+            this.vBNetToolStripMenuItem});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // cToolStripMenuItem
+            // 
+            this.cToolStripMenuItem.Name = "cToolStripMenuItem";
+            this.cToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cToolStripMenuItem.Text = "C#";
+            // 
+            // pythonToolStripMenuItem
+            // 
+            this.pythonToolStripMenuItem.Name = "pythonToolStripMenuItem";
+            this.pythonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pythonToolStripMenuItem.Text = "Python";
+            // 
+            // javaScriptToolStripMenuItem
+            // 
+            this.javaScriptToolStripMenuItem.Name = "javaScriptToolStripMenuItem";
+            this.javaScriptToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.javaScriptToolStripMenuItem.Text = "JavaScript";
+            // 
+            // objectiveCToolStripMenuItem
+            // 
+            this.objectiveCToolStripMenuItem.Name = "objectiveCToolStripMenuItem";
+            this.objectiveCToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.objectiveCToolStripMenuItem.Text = "Objective-C";
+            // 
+            // perlToolStripMenuItem
+            // 
+            this.perlToolStripMenuItem.Name = "perlToolStripMenuItem";
+            this.perlToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.perlToolStripMenuItem.Text = "Perl";
+            // 
+            // vBNetToolStripMenuItem
+            // 
+            this.vBNetToolStripMenuItem.Name = "vBNetToolStripMenuItem";
+            this.vBNetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vBNetToolStripMenuItem.Text = "VB.Net";
+            // 
+            // luaToolStripMenuItem
+            // 
+            this.luaToolStripMenuItem.Name = "luaToolStripMenuItem";
+            this.luaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.luaToolStripMenuItem.Text = "Lua";
+            // 
+            // pHPToolStripMenuItem
+            // 
+            this.pHPToolStripMenuItem.Name = "pHPToolStripMenuItem";
+            this.pHPToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pHPToolStripMenuItem.Text = "PHP";
+            // 
+            // cToolStripMenuItem1
+            // 
+            this.cToolStripMenuItem1.Name = "cToolStripMenuItem1";
+            this.cToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.cToolStripMenuItem1.Text = "C";
+            // 
+            // cToolStripMenuItem2
+            // 
+            this.cToolStripMenuItem2.Name = "cToolStripMenuItem2";
+            this.cToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.cToolStripMenuItem2.Text = "C++";
             // 
             // Form1
             // 
@@ -194,6 +297,17 @@
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
         private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem cToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem javaScriptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem luaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem objectiveCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pythonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem perlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pHPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vBNetToolStripMenuItem;
     }
 }
 
--- CodeEditor/Form1.cs
+++ CodeEditor/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CodeEditor
 {
@@ -15,6 +16,22 @@ namespace CodeEditor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Select A File To Open - Ensure You Match The Formats Supported";
+            file.Filter = "C# files|*.csproj;*.cs|VB.net|*.vb|HTML|*.html|XML|*.xml|SQL|*.sql|PHP|*.php|JavaScript Files|*.js|Lua Files|*.lua";
+            file.ShowDialog();
+
+            try
+            {
+                StreamReader streamReader = new StreamReader(file.FileName);
+                fastColoredTextBox1.Text = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+            catch {}
         }
     }
 }
