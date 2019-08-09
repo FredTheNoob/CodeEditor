dcb52160b782e877c01e6a2da5866d6f18725518
FredTheNoob <43958385+FredTheNoob@users.noreply.github.com>
2019-08-09 15:06:30 +0200

    Made GUI start for editor

--- CodeEditor/Form1.Designer.cs
+++ CodeEditor/Form1.Designer.cs
@@ -29,12 +29,171 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ccsprojToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textFiletxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pythonpyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileCTRLSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileAsCTRLSHIFTSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFileToolStripMenuItem,
+            this.openFileToolStripMenuItem,
+            this.saveFileCTRLSToolStripMenuItem,
+            this.saveFileAsCTRLSHIFTSToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // newFileToolStripMenuItem
+            // 
+            this.newFileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ccsprojToolStripMenuItem,
+            this.textFiletxtToolStripMenuItem,
+            this.pythonpyToolStripMenuItem});
+            this.newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            this.newFileToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.newFileToolStripMenuItem.Text = "New File";
+            // 
+            // ccsprojToolStripMenuItem
+            // 
+            this.ccsprojToolStripMenuItem.Name = "ccsprojToolStripMenuItem";
+            this.ccsprojToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ccsprojToolStripMenuItem.Text = "C# (.csproj)";
+            // 
+            // textFiletxtToolStripMenuItem
+            // 
+            this.textFiletxtToolStripMenuItem.Name = "textFiletxtToolStripMenuItem";
+            this.textFiletxtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.textFiletxtToolStripMenuItem.Text = "Text File (.txt)";
+            // 
+            // pythonpyToolStripMenuItem
+            // 
+            this.pythonpyToolStripMenuItem.Name = "pythonpyToolStripMenuItem";
+            this.pythonpyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pythonpyToolStripMenuItem.Text = "Python (.py)";
+            // 
+            // saveFileCTRLSToolStripMenuItem
+            // 
+            this.saveFileCTRLSToolStripMenuItem.Name = "saveFileCTRLSToolStripMenuItem";
+            this.saveFileCTRLSToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.saveFileCTRLSToolStripMenuItem.Text = "Save File (CTRL+S)";
+            // 
+            // saveFileAsCTRLSHIFTSToolStripMenuItem
+            // 
+            this.saveFileAsCTRLSHIFTSToolStripMenuItem.Name = "saveFileAsCTRLSHIFTSToolStripMenuItem";
+            this.saveFileAsCTRLSHIFTSToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.saveFileAsCTRLSHIFTSToolStripMenuItem.Text = "Save File As (CTRL+SHIFT+S)";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(179, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.CSharp;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 27);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(800, 423);
+            this.fastColoredTextBox1.TabIndex = 1;
+            this.fastColoredTextBox1.Text = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ccsprojToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textFiletxtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pythonpyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileCTRLSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileAsCTRLSHIFTSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
     }
 }
 
